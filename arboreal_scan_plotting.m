bd565d5ea5ab09d3e651cec4bdd0775cdb473825
Antoine <a.valera@ucl.ac.uk>
2022-05-23 11:27:44 +0200

    added missing figure title

--- arboreal_scan_plotting.m
+++ arboreal_scan_plotting.m
@@ -60,6 +60,7 @@ classdef arboreal_scan_plotting < handle
             
         function plot_original_data(obj)
             figure(1055);clf();plot(obj.t, obj.extracted_traces_conc,'Color',[0.9,0.9,0.9]); hold on
+            hold on;title('Original Data and average');set(gcf, 'Color', 'w')
             plot(obj.t, nanmean(obj.extracted_traces_conc,2),'r');            
         end
 
