e2cd901be566869a27243ac8f788b861bb52c8f3
Antoine <a.valera@ucl.ac.uk>
2022-05-23 11:27:04 +0200

    added a new function to plot many traces (eg wtith spine detection)

--- demo_script/plot_many_traces.m
+++ demo_script/plot_many_traces.m
@@ -1,11 +1,13 @@
+%% TODO: ADD DOC, SET SPACING FACTOR AS A INPUT PARAMETER
+
 function [traces, fig, offsets] = plot_many_traces(traces, fig, varargin)
     if nargin < 2  || isempty(fig)
-        fig = figure(); hold on;
+        fig         = figure(); hold on;
     elseif isnumeric(fig) || ishandle(fig)
         if isa(fig, 'matlab.graphics.axis.Axes') % subplot
             subplot(fig)
         else % fig
-            fig = figure(fig); hold on;
+            fig     = figure(fig); hold on;
         end
     else
         fig = [];
@@ -13,13 +15,18 @@ function [traces, fig, offsets] = plot_many_traces(traces, fig, varargin)
     %traces = smoothdata(squeeze(traces), 'gaussian',[5, 0]);
     
     if ndims(traces) > 2
-        traces = squeeze(traces);
+        traces      = squeeze(traces);
     end
     
-    spacing = nanmedian(rms(traces))/5;
-    n_traces = size(traces, 2);
-    offsets = linspace(0,n_traces*spacing,n_traces);    
-    traces = traces + offsets;
+    spacing_factor  = 1    
+    spacing         = nanmedian(rms(traces))/spacing_factor;    
+    m               = nanmin(traces(:));
+    traces(:,1)     = m;
+    traces(:,end)   = m;
+    
+    n_traces        = size(traces, 2);
+    offsets         = linspace(0,n_traces*spacing,n_traces);    
+    traces          = traces + offsets;
     if ~isempty(fig)
     	plot(1:size(traces, 1), traces, varargin{:});ylim([nanmin(traces(:)),nanmax(traces(:))])
     end
