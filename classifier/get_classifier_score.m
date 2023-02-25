54b8a44f20b45e7ddd0da2026bf289e207ac91ce
TheBeast2 <avalera@unistra.fr>
2023-02-25 10:36:37 +0000

    added missing default input

--- classifier/get_classifier_score.m
+++ classifier/get_classifier_score.m
@@ -1,4 +1,7 @@
-function [score, TPR, TNR, MCC] = get_classifier_score(y_test, y_predict)
+function [score, TPR, TNR, MCC] = get_classifier_score(y_test, y_predict, Weights)
+    if nargin < 3 || isempty(Weights)
+        Weights = ones(size(y_test));
+    end
     y_test = y_test';
     
     if islogical(y_test)
@@ -32,6 +35,6 @@ function [score, TPR, TNR, MCC] = get_classifier_score(y_test, y_predict)
             warning(['Prediction returned ',num2str(unique(y_predict)),' for all prediction. It was replaced by a random set of values to get correlation score '])
             y_predict = rand(size(y_predict)) * range(y_test);
         end
-        [score, TPR, TNR, MCC] = deal(corr(y_test,y_predict)*100);
+        [score, TPR, TNR, MCC] = deal(corr(y_test,y_predict)*100);      
     end
 end
